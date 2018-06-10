69b9c49ff745901ee4c64aae61469451fa3dd3bc
Michael Froehlich <m.froehlich@tum.de>
2018-06-11 00:23:15 +0200

    Add LaunchViewController

---
+++ tip/Presentation/LaunchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchViewController.swift
+//  tip
+//
+//  Created by Michael Fröhlich on 11.06.18.
+//  Copyright © 2018 Michael Fröhlich. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    private static let segueIdentifier = "showMainScreen"
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        performSegue(withIdentifier: LaunchViewController.segueIdentifier, sender: self)
+    }
+}
